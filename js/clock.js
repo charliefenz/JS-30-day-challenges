087767b42cfbf88cb7e8ac5d052496123789e2de
charliefenz <fentecarlos@gmail.com>
2022-02-17 13:12:14 +0100

    Create function to replace classes in charge of rotation

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,25 @@
-const styleSheet = getUniqueStyleSheet('clockCSS');
-
 function getUniqueStyleSheet(styleSheetTitle) {
     const styleSheet = Array.from(document.styleSheets).filter(item => item.title === styleSheetTitle);
     const styleSheetUnit = styleSheet[0];
     return styleSheetUnit;
 }
+
+function changeRotation(styleSheet, cssSelector, unit) {
+    const styleSheetCSSRules = styleSheet.cssRules;
+    const cssRuleIndex = findCSSRuleIndex(styleSheetCSSRules, cssSelector);
+    styleSheet.deleteRule(cssRuleIndex);
+    styleSheet.insertRule(`
+        ${cssSelector} {
+            transform: rotate(${unit});
+        }
+    `)
+}
+
+function findCSSRuleIndex(styleSheetCSSRules, cssSelector) {
+    return Array.from(styleSheetCSSRules).findIndex(item => item.selectorText === cssSelector);
+}
+
+const styleSheet = getUniqueStyleSheet('clockCSS');
+changeRotation(styleSheet, '.seconds-rotation', '0deg');
 
 
