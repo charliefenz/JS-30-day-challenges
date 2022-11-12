cce2aa53b15a2f516e5322d7bd0b7c3a0d05a33c
charliefenz <fentecarlos@gmail.com>
2022-11-12 07:40:30 +0100

    Add functions to trigger animations

--- js/flex-panels-image-gallery.js
+++ js/flex-panels-image-gallery.js
@@ -1,7 +1,19 @@
 function hover(element) {
+    const children = element.children;
+
+    children[0].classList.remove('top-exit');
+    children[2].classList.remove('bottom-exit');
+    children[0].classList.add('top-entrance');
+    children[2].classList.add('bottom-entrance');
     element.classList.add('selected');
 }
 
 function dehover(element) {
+    const children = element.children;
+
+    children[0].classList.remove('top-entrance');
+    children[2].classList.remove('bottom-entrance');
+    children[0].classList.add('top-exit');
+    children[2].classList.add('bottom-exit');
     element.classList.remove('selected');
 }
